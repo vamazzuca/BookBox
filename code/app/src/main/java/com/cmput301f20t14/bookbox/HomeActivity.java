9f8e638d1512e8903895dc61b91b464ad98683c7
Alvrrick <35272187+Alvrrick@users.noreply.github.com>
2020-10-24 18:44:50 -0600

    Bottom Navbar implementation
    -implementation bottom nav bar
    -added ViewProfileActitivty
    -added ViewNotifiactionActivity
    -added ViewRequestsActivity

--- code/app/src/main/java/com/cmput301f20t14/bookbox/HomeActivity.java
+++ code/app/src/main/java/com/cmput301f20t14/bookbox/HomeActivity.java
@@ -27,11 +27,16 @@
  */
 package com.cmput301f20t14.bookbox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * This shows the Home Menu with a task bar at the bottom
@@ -50,8 +55,48 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
+        setContentView(R.layout.activity_home);
 
         Log.d("Logged in!", "Yay!");
+
+        bottomNavigationView();
+
     }
+
+    /**
+     * Implementation of the bottom navigation bar for switching to different
+     * activity views, such as home, profile, notifications and lists
+     * References: https://www.youtube.com/watch?v=JjfSjMs0ImQ&feature=youtu.be
+     * @author Alex Mazzuca
+     * @version 2020.10.24
+     */
+    private void bottomNavigationView(){
+        //Home Navigation bar implementation
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+        bottomNavigationView.setSelectedItemId(R.id.home_bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.lists_bottom_nav:
+                        startActivity(new Intent(getApplicationContext(), ViewRequestsActivity.class ));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home_bottom_nav:
+                        return true;
+                    case R.id.notification_bottom_nav:
+                        startActivity(new Intent(getApplicationContext(), ViewNotificationsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile_bottom_nav:
+                        startActivity(new Intent(getApplicationContext(), ViewProfileActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
 }
